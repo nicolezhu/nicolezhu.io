cc3d1be39ca2e4b3f2653481d757f7dc66dc34a9
Nicole Zhu <nzhu94@gmail.com>
2016-03-31 00:42:49 -0500

    a bunch of refactoring

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
 // tabletop
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1zmfoTcFIxDtF_QHff5WzYkiSVp1Aq3fzBFVe5vDiUIw/pubhtml';
+var spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1zmfoTcFIxDtF_QHff5WzYkiSVp1Aq3fzBFVe5vDiUIw/pubhtml';
+var talks_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Hqhkn6tLeKT_eVHoYwuZofzVh2WrxSAa6-pcP9Z-CHY/pubhtml';
 function init() {
-		Tabletop.init( { key: public_spreadsheet_url,
-                   callback: readData,
-                   simpleSheet: true } )
+		Tabletop.init({ 
+            key: spreadsheet_url,
+            callback: readData,
+            simpleSheet: false 
+        });
 	}
 
 // read data
-var projects;
+var projects, talks;
 function readData(data, tabletop) { 
-	projects = data;
-	console.log(projects);
-	showGrid();
+	projects = data.Work.elements;
+    talks = data.Talks.elements;
+    console.log(projects);
+	showWorkGrid();
+    showTalksGrid();
 }
 
-function showGrid() {
+function showWorkGrid() {
 	for (i=0; i < projects.length; i++) {
-		$('.grid').append('<div class="item"><div class="element-top"><a href="'+ projects[i].link + '"><img src="' + projects[i].screenshot + '" /></a><div class="details"><h4>' + projects[i].project + '</h4><p class="org"><strong>' + projects[i].organization + '</strong></p><p class="description">' + projects[i].description + '</p></div></div></div>');
+		$('#work-grid').append('<div class="item"><div class="element-top"><a href="'+ projects[i].link + '"><img src="' + projects[i].screenshot + '" /></a><div class="details"><h4>' + projects[i].project + '</h4><p class="org"><strong>' + projects[i].organization + '</strong></p><p class="description">' + projects[i].description + '</p></div></div></div>');
 	}
 }
 
-function scrollTo(section) {
-	var sticky_nav = $('.main-nav').outerHeight();
-  $('html,body').animate({scrollTop: $(section).offset().top - sticky_nav}, 800);
+function showTalksGrid() {
+    for (i=0; i < talks.length; i++) {
+        $('#talks-grid').append('<div class="item"><div class="element-top"><a href="'+ talks[i].link + '"><img src="' + talks[i].screenshot + '" /></a><div class="details"><h4>' + talks[i].talk + '</h4><p class="org"><strong>' + talks[i].purpose + '</strong></p><p class="description">' + talks[i].description + '</p></div></div></div>');
+    }
 }
 
 $(document).ready(function() {
@@ -38,6 +44,32 @@ $(document).ready(function() {
 	    mn.removeClass(mns);
 	  }
 	});
+
+    // navigation
+    var scrollToSection = function(section) {
+        var sticky_nav = $('.main-nav').outerHeight();
+        $('html, body').animate({ scrollTop: $(section).offset().top - sticky_nav }, 800);
+    }
+
+    $("#nav-about").click(function() {
+        scrollToSection('.about');
+    });
+
+    $("#nav-work").click(function() {
+        scrollToSection('.work');
+    });
+
+    $("#nav-writing").click(function() {
+        scrollToSection('.writing');
+    });
+
+    $("#nav-talks").click(function() {
+        scrollToSection('.talks');
+    });
+
+    $("#nav-contact").click(function() {
+        scrollToSection('.contact');
+    });
 
 	init();
 });
